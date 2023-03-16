d803cef7cfd42fd73e6eaa8b6c8598deb74d9054
413C5 <124840787+413C5@users.noreply.github.com>
2023-03-16 14:10:16 -0600

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,285 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmPrincipal
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.label1 = New System.Windows.Forms.Label()
+        Me.label2 = New System.Windows.Forms.Label()
+        Me.label3 = New System.Windows.Forms.Label()
+        Me.label4 = New System.Windows.Forms.Label()
+        Me.label5 = New System.Windows.Forms.Label()
+        Me.label6 = New System.Windows.Forms.Label()
+        Me.label7 = New System.Windows.Forms.Label()
+        Me.label8 = New System.Windows.Forms.Label()
+        Me.label9 = New System.Windows.Forms.Label()
+        Me.label10 = New System.Windows.Forms.Label()
+        Me.txtNumero = New System.Windows.Forms.TextBox()
+        Me.txtCadena = New System.Windows.Forms.TextBox()
+        Me.btnMostrar = New System.Windows.Forms.Button()
+        Me.btnBuscar = New System.Windows.Forms.Button()
+        Me.btnSalir = New System.Windows.Forms.Button()
+        Me.label11 = New System.Windows.Forms.Label()
+        Me.txtDescripcion = New System.Windows.Forms.TextBox()
+        Me.labEncontrado = New System.Windows.Forms.Label()
+        Me.labPrimera = New System.Windows.Forms.Label()
+        Me.labUltima = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'label1
+        '
+        Me.label1.AutoSize = True
+        Me.label1.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.label1.Location = New System.Drawing.Point(31, 31)
+        Me.label1.Name = "label1"
+        Me.label1.Size = New System.Drawing.Size(223, 20)
+        Me.label1.TabIndex = 0
+        Me.label1.Text = "Nombre de componentes UAR"
+        '
+        'label2
+        '
+        Me.label2.AutoSize = True
+        Me.label2.Location = New System.Drawing.Point(31, 63)
+        Me.label2.Name = "label2"
+        Me.label2.Size = New System.Drawing.Size(142, 20)
+        Me.label2.TabIndex = 1
+        Me.label2.Text = "1. Identificador UAR"
+        '
+        'label3
+        '
+        Me.label3.AutoSize = True
+        Me.label3.Location = New System.Drawing.Point(31, 92)
+        Me.label3.Name = "label3"
+        Me.label3.Size = New System.Drawing.Size(377, 20)
+        Me.label3.TabIndex = 2
+        Me.label3.Text = "2. Una Descripción Breve de los Aspectos de Usabilidad"
+        '
+        'label4
+        '
+        Me.label4.AutoSize = True
+        Me.label4.Location = New System.Drawing.Point(31, 125)
+        Me.label4.Name = "label4"
+        Me.label4.Size = New System.Drawing.Size(217, 20)
+        Me.label4.TabIndex = 3
+        Me.label4.Text = "3. Las Evidencias de un Aspecto"
+        '
+        'label5
+        '
+        Me.label5.AutoSize = True
+        Me.label5.Location = New System.Drawing.Point(31, 157)
+        Me.label5.Name = "label5"
+        Me.label5.Size = New System.Drawing.Size(217, 20)
+        Me.label5.TabIndex = 4
+        Me.label5.Text = "4. La Explicación de un Aspecto"
+        '
+        'label6
+        '
+        Me.label6.AutoSize = True
+        Me.label6.Location = New System.Drawing.Point(31, 189)
+        Me.label6.Name = "label6"
+        Me.label6.Size = New System.Drawing.Size(493, 20)
+        Me.label6.TabIndex = 5
+        Me.label6.Text = "5. La Severidad del Problema o el Beneficio de una Característica Positiva"
+        '
+        'label7
+        '
+        Me.label7.AutoSize = True
+        Me.label7.Location = New System.Drawing.Point(31, 223)
+        Me.label7.Name = "label7"
+        Me.label7.Size = New System.Drawing.Size(337, 20)
+        Me.label7.TabIndex = 6
+        Me.label7.Text = "6. Las Posibles Soluciones y los Costos Potenciales"
+        '
+        'label8
+        '
+        Me.label8.AutoSize = True
+        Me.label8.Location = New System.Drawing.Point(31, 257)
+        Me.label8.Name = "label8"
+        Me.label8.Size = New System.Drawing.Size(397, 20)
+        Me.label8.TabIndex = 7
+        Me.label8.Text = "7. La Relación que Tiene con Otros Aspectos de Usabilidad"
+        '
+        'label9
+        '
+        Me.label9.AutoSize = True
+        Me.label9.Location = New System.Drawing.Point(31, 327)
+        Me.label9.Name = "label9"
+        Me.label9.Size = New System.Drawing.Size(135, 20)
+        Me.label9.TabIndex = 8
+        Me.label9.Text = "Ingresa un número:"
+        '
+        'label10
+        '
+        Me.label10.AutoSize = True
+        Me.label10.Location = New System.Drawing.Point(31, 373)
+        Me.label10.Name = "label10"
+        Me.label10.Size = New System.Drawing.Size(199, 20)
+        Me.label10.TabIndex = 9
+        Me.label10.Text = "Ingresa una cadena a buscar:"
+        '
+        'txtNumero
+        '
+        Me.txtNumero.Location = New System.Drawing.Point(239, 327)
+        Me.txtNumero.Name = "txtNumero"
+        Me.txtNumero.Size = New System.Drawing.Size(125, 27)
+        Me.txtNumero.TabIndex = 0
+        '
+        'txtCadena
+        '
+        Me.txtCadena.Location = New System.Drawing.Point(239, 373)
+        Me.txtCadena.Name = "txtCadena"
+        Me.txtCadena.Size = New System.Drawing.Size(125, 27)
+        Me.txtCadena.TabIndex = 2
+        '
+        'btnMostrar
+        '
+        Me.btnMostrar.Location = New System.Drawing.Point(383, 327)
+        Me.btnMostrar.Name = "btnMostrar"
+        Me.btnMostrar.Size = New System.Drawing.Size(94, 29)
+        Me.btnMostrar.TabIndex = 1
+        Me.btnMostrar.Text = "&Mostrar"
+        Me.btnMostrar.UseVisualStyleBackColor = True
+        '
+        'btnBuscar
+        '
+        Me.btnBuscar.Location = New System.Drawing.Point(383, 373)
+        Me.btnBuscar.Name = "btnBuscar"
+        Me.btnBuscar.Size = New System.Drawing.Size(94, 29)
+        Me.btnBuscar.TabIndex = 3
+        Me.btnBuscar.Text = "&Buscar"
+        Me.btnBuscar.UseVisualStyleBackColor = True
+        '
+        'btnSalir
+        '
+        Me.btnSalir.Location = New System.Drawing.Point(826, 373)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(94, 29)
+        Me.btnSalir.TabIndex = 4
+        Me.btnSalir.Text = "&Salir"
+        Me.btnSalir.UseVisualStyleBackColor = True
+        '
+        'label11
+        '
+        Me.label11.AutoSize = True
+        Me.label11.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.label11.Location = New System.Drawing.Point(552, 31)
+        Me.label11.Name = "label11"
+        Me.label11.Size = New System.Drawing.Size(247, 20)
+        Me.label11.TabIndex = 15
+        Me.label11.Text = "Descripción del componente UAR:"
+        '
+        'txtDescripcion
+        '
+        Me.txtDescripcion.Location = New System.Drawing.Point(552, 63)
+        Me.txtDescripcion.Multiline = True
+        Me.txtDescripcion.Name = "txtDescripcion"
+        Me.txtDescripcion.ReadOnly = True
+        Me.txtDescripcion.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtDescripcion.Size = New System.Drawing.Size(368, 180)
+        Me.txtDescripcion.TabIndex = 16
+        Me.txtDescripcion.TabStop = False
+        '
+        'labEncontrado
+        '
+        Me.labEncontrado.AutoSize = True
+        Me.labEncontrado.Location = New System.Drawing.Point(552, 257)
+        Me.labEncontrado.Name = "labEncontrado"
+        Me.labEncontrado.Size = New System.Drawing.Size(108, 20)
+        Me.labEncontrado.TabIndex = 17
+        Me.labEncontrado.Text = "Encontrado en:"
+        '
+        'labPrimera
+        '
+        Me.labPrimera.AutoSize = True
+        Me.labPrimera.Location = New System.Drawing.Point(552, 293)
+        Me.labPrimera.Name = "labPrimera"
+        Me.labPrimera.Size = New System.Drawing.Size(123, 20)
+        Me.labPrimera.TabIndex = 18
+        Me.labPrimera.Text = "Primera posición:"
+        Me.labPrimera.Visible = False
+        '
+        'labUltima
+        '
+        Me.labUltima.AutoSize = True
+        Me.labUltima.Location = New System.Drawing.Point(552, 336)
+        Me.labUltima.Name = "labUltima"
+        Me.labUltima.Size = New System.Drawing.Size(116, 20)
+        Me.labUltima.TabIndex = 19
+        Me.labUltima.Text = "Última posicion:"
+        Me.labUltima.Visible = False
+        '
+        'frmPrincipal
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnSalir
+        Me.ClientSize = New System.Drawing.Size(939, 450)
+        Me.Controls.Add(Me.labUltima)
+        Me.Controls.Add(Me.labPrimera)
+        Me.Controls.Add(Me.labEncontrado)
+        Me.Controls.Add(Me.txtDescripcion)
+        Me.Controls.Add(Me.label11)
+        Me.Controls.Add(Me.btnSalir)
+        Me.Controls.Add(Me.btnBuscar)
+        Me.Controls.Add(Me.btnMostrar)
+        Me.Controls.Add(Me.txtCadena)
+        Me.Controls.Add(Me.txtNumero)
+        Me.Controls.Add(Me.label10)
+        Me.Controls.Add(Me.label9)
+        Me.Controls.Add(Me.label8)
+        Me.Controls.Add(Me.label7)
+        Me.Controls.Add(Me.label6)
+        Me.Controls.Add(Me.label5)
+        Me.Controls.Add(Me.label4)
+        Me.Controls.Add(Me.label3)
+        Me.Controls.Add(Me.label2)
+        Me.Controls.Add(Me.label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "frmPrincipal"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Componentes UAR"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents label1 As Label
+    Friend WithEvents label2 As Label
+    Friend WithEvents label3 As Label
+    Friend WithEvents label4 As Label
+    Friend WithEvents label5 As Label
+    Friend WithEvents label6 As Label
+    Friend WithEvents label7 As Label
+    Friend WithEvents label8 As Label
+    Friend WithEvents label9 As Label
+    Friend WithEvents label10 As Label
+    Friend WithEvents txtNumero As TextBox
+    Friend WithEvents txtCadena As TextBox
+    Friend WithEvents btnMostrar As Button
+    Friend WithEvents btnBuscar As Button
+    Friend WithEvents btnSalir As Button
+    Friend WithEvents label11 As Label
+    Friend WithEvents txtDescripcion As TextBox
+    Friend WithEvents labEncontrado As Label
+    Friend WithEvents labPrimera As Label
+    Friend WithEvents labUltima As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,220 @@
+﻿Public Class frmPrincipal
+#Region "Atributos"
+    Private _veces As Integer
+    Dim componentesUAR() As String = {"Con el objetivo de que se archive el reporte, este debe contar con un identificador único. Este identificador se puede formar con números y letras, para el caso en que más de una persona esté trabajando en este proyecto o para el caso que se esté utilizando más de una técnica de análisis. Por ejemplo, si Carlos Pérez y Judith Taméz están llevando este análisis, podemos crear un identificador como CP10 o JT8. Si se están utilizando los estudios de evaluación heurística y usabilidad de pensamiento en voz alta, los identificadores pudieran ser HE4 o TA9.",
+        "Esta descripción será el ""nombre"" de la UAR que usarás cuando la menciones como relación con otra UAR. Asigna el nombre más corto que puedas (de tres a cinco palabras) asegurando que describa el aspecto y además que se distinga del resto de los aspectos del sistema.",
+        "Este es el material de soporte objetivo que justifica el aspecto que identificaste y especifica que merece un reporte. Esta sección debe contener la información suficiente para que la persona que lea este UAR comprenda la razón que hizo que se creara este reporte. Para un reporte HE, por ejemplo, las evidencias pueden consistir en una imagen que represente desorden y la heurística que habla de diseño estético y minimalista. O puede consistir en una lista de elementos de menú que no tienen un atajo y la heurística que habla de incluir atajos. En un estudio de pensamiento en voz alta la prueba consiste, por lo general de lo que se ve en pantalla ( una impresión de la pantalla o una descripción), las acciones que realizó el usuario (teclado, movimientos de mouse), lo que hizo el sistema como respuesta a las acciones del usuario, y lo que dijo el usuario. Si cuentas con un video o con la posibilidad de editarlo, puedes incluir una animación breve.",
+        "Esta es la interpretación de la evidencia. Esto es, para la prueba de usabilidad de pensamiento en voz alta, la razón por la cual pasó lo que pasó, o, para una HE, la razón por la cual crees que se diseñó de la manera en que está diseñada. Aquí se pueden incluir leyendas como: ""la etiqueta del botón XYZ, es un término de programación estándar, pero el usuario, al parecer, no estaba familiarizado con este término"" o ""el sistema estaba en modo de edición, pero el usuario pensó que estaba en modo ejecutable porque no hay una diferencia muy marcada entre estos modos en la pantalla."" (Esto se debe expresar de manera que analice la situación que sucedió con el aspecto del sistema, NO de una manera que de insinúe que se debe hacer una evaluación del desarrollador o del usuario.)",
+        "Aquí debes especificar, según tu razonamiento, el grado de importancia que tiene este problema para que se resuelva o para mantenerlo como característica positiva. Aquí puedes incluir información referente a la frecuencia con la cual se le presentará al usuario, si acaso crees que aprenda el usuario como funciona este problema, si crees que el problema le afecta más a los usuarios nuevos, no frecuentes o con experiencia, entre otros.",
+        "Si el aspecto representa un problema (opuesto a una característica positiva que debe permanecer en la siguiente versión del software), aquí es donde se debe proponer una solución. No es necesario contar con una solución al momento que identificaste un problema—podrá darse el caso en que después de analizar toda la interfaz, muchos problemas estén relacionados y se puedan solucionar todos haciendo algunos cambios generales.",
+        "Muchas veces los UAR están relacionados entre sí. Aquí es donde se documenta con cual UAR está relacionado cada uno y un breve enunciado que indica la forma en que está relacionado. Asegúrate que todas las UAR relacionadas documenten al resto de las UAR relacionadas. Por decir, Si el UAR #1 está relacionado con el UAR #30, el UAR #30 debe indicar en esta sección, la relación que tiene con UAR #1 y el UAR #1 debe indicar la relación que tiene con la UAR #30. (El error más común es que en la UAR más reciente indiques la relación con las UAR anteriores, pero no te regreses para documentar en las UAR previas su relación con la UAR más reciente)."}
+
+    Public Property Veces As Integer
+        Set(value As Integer)
+            'Validar que el valor sea permitido
+            If value < 0 Then
+                value = 0
+            End If
+
+            'Validar si hay un cambio en el valor
+            If value <> _veces Then
+                _veces = value
+                'Cambiar objetos que dependen del valor
+                If _veces = 0 Then
+                    labPrimera.Visible = False
+                    labUltima.Visible = False
+                Else
+                    labPrimera.Visible = True
+                    labUltima.Visible = True
+                End If
+            End If
+        End Set
+        Get
+            Return _veces
+        End Get
+    End Property
+#End Region
+
+#Region "Metodos"
+
+    ''' <summary>
+    ''' Asigna un texto UAR en base al índice proveido 
+    ''' </summary>
+    ''' <param name="valor"></param>
+    Private Sub AsignarUAR(ByVal valor As String)
+        Dim mensaje, estilo, titulo As String
+        mensaje = "Teclea un valor entre 1 y 7"
+        estilo = vbCritical
+        titulo = "Error"
+        Try
+            'Rango 1-7
+            Dim indice As Integer = Integer.Parse(valor)
+            Select Case indice
+                Case 1
+                    txtDescripcion.Text = componentesUAR(0)
+                Case 2
+                    txtDescripcion.Text = componentesUAR(1)
+                Case 3
+                    txtDescripcion.Text = componentesUAR(2)
+                Case 4
+                    txtDescripcion.Text = componentesUAR(3)
+                Case 5
+                    txtDescripcion.Text = componentesUAR(4)
+                Case 6
+                    txtDescripcion.Text = componentesUAR(5)
+                Case 7
+                    txtDescripcion.Text = componentesUAR(6)
+
+                    'Índice fuera de rango
+                Case Else
+                    MsgBox(mensaje, estilo, titulo)
+                    txtNumero.SelectAll()
+                    txtNumero.Focus()
+            End Select
+
+            'Cuando el índice que se provee es vacio o una cadena
+        Catch ex As Exception
+            estilo = vbExclamation
+            MsgBox(mensaje, estilo, titulo)
+            txtNumero.SelectAll()
+            txtNumero.Focus()
+        End Try
+    End Sub
+
+    ''' <summary>
+    ''' Limpia los textboxes utilizados, asi como el hacer invisibles los labels de instancias
+    ''' </summary>
+    Private Sub Limpiar()
+        txtNumero.Clear()
+        txtCadena.Clear()
+        txtDescripcion.Clear()
+        labPrimera.Visible = False
+        labUltima.Visible = False
+    End Sub
+
+    ''' <summary>
+    ''' Busca una cadena dentro de otra cadena, indicando las veces en que se encuentra la cadena, asi como la primera y ultima instancia 
+    ''' de donde se encuentran
+    ''' </summary>
+    ''' <param name="palabra"></param>
+    ''' <param name="texto"></param>
+    Private Sub Buscar(ByVal palabra As String, ByVal texto As String)
+        Dim prueba = 0
+        Dim ultimaPosicion As Integer
+        Dim cursor As Integer = LCase(texto).IndexOf(LCase(palabra), 0)
+        Dim respuesta As Integer
+        Dim mensaje, estilo, titulo
+
+        Try
+            'No se ha asignado un índice de descripción UAR
+            If String.IsNullOrEmpty(texto) = True Then
+                Throw New ArgumentException()
+            End If
+
+            'En caso de encontrar coincidencia, se inicia la busqueda
+            If cursor <> -1 Then
+
+                'Si devuelve -1 es porque no encontro coincidencia
+                If String.IsNullOrEmpty(txtCadena.Text) = True Then
+                    Throw New Exception()
+                End If
+
+                'Primera ocurrencia de la palabra
+                labPrimera.Text = "Primera posición:" & cursor
+                labPrimera.Visible = True
+
+                'Se recorre toda la cadena, para contar cuantas veces se encuentra la palabra
+                While cursor <> -1
+                    Veces += 1
+                    ultimaPosicion = cursor
+                    cursor = LCase(texto).IndexOf(LCase(palabra), cursor + 1)
+                End While
+
+                'Ultima ocurrencia de la palabra
+                labUltima.Text = "Última posición:" & ultimaPosicion
+                labUltima.Visible = True
+
+                'En caso de haber encontrado una palabra, se pregunta si se quiere hacer otra busqueda
+                Beep()
+                mensaje = "Se ha encontrado la palabra " & Veces & " veces.¿Desea buscar nuevamente?"
+                estilo = vbYesNo + vbQuestion
+                titulo = "Palabra encontrada"
+
+                'Si la busqueda se quiere realizar en la misma descripción (exito)
+                respuesta = MsgBox(mensaje, estilo, titulo)
+                If respuesta = vbYes Then
+                    txtCadena.SelectAll()
+                    txtCadena.Focus()
+
+                    'Si la busqueda se quiere hacer en otra descripción (exito)
+                Else
+                    Limpiar()
+                    'txtDescripcion.Clear()
+                    txtNumero.Focus()
+                    mensaje = "Ingrese nuevamente información en los campos"
+                    estilo = vbInformation
+                    titulo = "Ingresar datos"
+                    MsgBox(mensaje, estilo, titulo)
+                End If
+
+                'Si la busqueda se quiere hacer en la misma descripción (sin coincidencia)
+            Else
+                mensaje = "No se encontro la palabra.¿Desea realizar otra busqueda en la misma descripción?"
+                estilo = vbYesNo + vbQuestion
+                titulo = "Búsqueda"
+
+                Beep()
+                respuesta = MsgBox(mensaje, estilo, titulo)
+                If respuesta = vbYes Then
+                    txtCadena.Clear()
+                    txtCadena.Focus()
+
+                    'Si la busqueda se quiere hacer en otra descripción (sin coincidencia)
+                Else
+                    Limpiar()
+                    'txtDescripcion.Clear()
+                    txtNumero.Focus()
+                End If
+            End If
+
+            'Sin descripcion UAR
+        Catch ex As ArgumentException
+            mensaje = "Seleccione una descripción de UAR"
+            estilo = vbCritical
+            titulo = "Sin descripción UAR"
+
+            'Pasar enfoque al boton Mostrar
+            If String.IsNullOrEmpty(txtNumero.Text) = True Then
+                MsgBox(mensaje, estilo, titulo)
+                txtNumero.Focus()
+            Else
+                btnMostrar.Select()
+            End If
+
+            'Sin palabra clave
+        Catch ex As Exception
+            mensaje = "Ingrese una palabra clave"
+            estilo = vbCritical
+            titulo = "Palabra clave"
+
+            MsgBox(mensaje, estilo, titulo)
+            txtCadena.Focus()
+        End Try
+    End Sub
+
+#End Region
+#Region "Eventos"
+    Private Sub btnMostrar_Click(sender As Object, e As EventArgs) Handles btnMostrar.Click
+        AsignarUAR(txtNumero.Text)
+    End Sub
+
+    Private Sub btnBuscar_Click(sender As Object, e As EventArgs) Handles btnBuscar.Click
+        Veces = 0
+        Buscar(txtCadena.Text, txtDescripcion.Text)
+    End Sub
+
+    Private Sub btnSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
+        End
+    End Sub
+#End Region
+End Class
